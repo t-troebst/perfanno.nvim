516fcc7ad2d2482ee306c759f8b255744f2aaa2e
orbital <orbital@holgerines.de>
2022-02-15 20:45:00 -0800

    Add comments to telescope extension

--- lua/telescope/_extensions/perfanno.lua
+++ lua/telescope/_extensions/perfanno.lua
@@ -1,4 +1,5 @@
 --- Telescope pickers to find hottest lines in the code base.
+-- TODO: Allow using different events than the current one.
 
 -- All the telescope stuff...
 local telescope = require("telescope")
@@ -13,6 +14,9 @@ local annotate = require("perfanno.annotate")
 local config = require("perfanno.config")
 local find_hottest = require("perfanno.find_hottest")
 
+--- Creates a telescope finder based on a table of lines / symbols.
+-- @param entries Table to be used.
+-- @return Returns the telescope finder.
 local function finder_from_table(entries)
     local cg = callgraph.callgraphs[config.selected_event]
     local opts = {results = entries}
@@ -37,6 +41,10 @@ local function finder_from_table(entries)
     return finders.new_table(opts)
 end
 
+--- Provides a telescope previeer that calls "annotate_fn" on each buffer to annotate it.
+-- @param annotate_fn Function that takes a buffer number and a file, gets called whenever a new
+--        file is loaded into that buffer.
+-- @return Returns the telescope previewer.
 local function annotated_previewer(annotate_fn)
     return previewers.new_buffer_previewer {
         define_preview = function(self, entry, _)
@@ -44,6 +52,8 @@ local function annotated_previewer(annotate_fn)
                 return
             end
 
+            -- This is the recommended caching mechanism for telescope. This way we don't need to
+            -- reannotate the same buffer while we are switching between lines.
             if self.state.bufname ~= entry.path then
                 local opts = {
                     callback = function(bufnr)
@@ -69,6 +79,9 @@ local function annotated_previewer(annotate_fn)
     }
 end
 
+--- Annotates a given buffer assuming it displays the given file.
+-- @param bufnr Buffer number of buffer to annotate.
+-- @param file File that should be used to look up annotations.
 local function annotate_file(bufnr, file)
     local cg = callgraph.callgraphs[config.selected_event]
 
@@ -81,6 +94,7 @@ local function annotate_file(bufnr, file)
     end
 end
 
+--- Opens telescope finder to find hottest lines in the project.
 local function find_hottest_lines()
     perfanno.with_event(function()
         local entries = find_hottest.hottest_lines_table(config.selected_event)
@@ -94,6 +108,7 @@ local function find_hottest_lines()
     end)
 end
 
+--- Opens telescope finder to find hottest symbols (functions) in the project.
 local function find_hottest_symbols()
     perfanno.with_event(function()
         local entries = find_hottest.hottest_symbols_table(config.selected_event)
@@ -107,6 +122,7 @@ local function find_hottest_symbols()
     end)
 end
 
+--- Opens telescope finder to find the hottest callers of the function containing the cursor.
 local function find_hottest_callers_function()
     perfanno.with_event(function()
         local entries = find_hottest.hottest_callers_function_table(config.selected_event)
@@ -124,6 +140,7 @@ local function find_hottest_callers_function()
     end)
 end
 
+--- Opens telescope finder to find the hottest callers of current visual selection.
 local function find_hottest_callers_selection()
     perfanno.with_event(function()
         local entries = find_hottest.hottest_callers_selection_table(config.selected_event)
