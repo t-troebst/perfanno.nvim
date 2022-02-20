552fdd5e255e164328a953627898ca395f300300
orbital <orbital@holgerines.de>
2022-02-19 18:17:27 -0800

    Allow traversal of the call graph inside the telescope finders

--- lua/telescope/_extensions/perfanno.lua
+++ lua/telescope/_extensions/perfanno.lua
@@ -7,6 +7,8 @@ local pickers = require("telescope.pickers")
 local previewers = require("telescope.previewers")
 local finders = require("telescope.finders")
 local tconf = require("telescope.config").values
+local actions = require("telescope.actions")
+local state = require("telescope.actions.state")
 
 local perfanno = require("perfanno")
 local callgraph = require("perfanno.callgraph")
@@ -30,6 +32,7 @@ local function finder_from_table(entries, total_count)
 
         return {
             lnum = entry.linenr or 1,
+            lnum_end = entry.linenr_end or 1,
             path = entry.file or "",
             display = display,
             ordinal = display,  -- TODO: better ordinal?
@@ -79,6 +82,73 @@ local function annotated_previewer(annotate_fn)
     }
 end
 
+local pa_actions = {}
+
+--- Telescope action that changes the current finder to the hottest callers of the selected entry.
+-- @param bufnr Buffer number of the prompt.
+function pa_actions.hottest_callers(bufnr)
+    local selection = state.get_selected_entry()
+
+    if selection.path == "" then
+        vim.notify("No path associated to this entry!")
+        return
+    end
+
+    local new_entries, new_count =
+        find_hottest.hottest_callers_table(config.selected_event, selection.path,
+                                           selection.lnum, selection.lnum_end)
+    local finder = finder_from_table(new_entries, new_count)
+    local picker = state.get_current_picker(bufnr)
+
+    picker:refresh(finder, {reset_prompt = true})
+end
+
+--- Telescope action that changes the current finder to the hottest callees of the selected entry.
+-- @param bufnr Buffer number of the prompt.
+function pa_actions.hottest_callees(bufnr)
+    local selection = state.get_selected_entry()
+
+    if selection.path == "" then
+        vim.notify("No path associated to this entry!")
+        return
+    end
+
+    local new_entries, new_count =
+        find_hottest.hottest_callees_table(config.selected_event, selection.path,
+                                           selection.lnum, selection.lnum_end)
+    local finder = finder_from_table(new_entries, new_count)
+    local picker = state.get_current_picker(bufnr)
+
+    picker:refresh(finder, {reset_prompt = true})
+end
+
+local popts = {
+    mappings = {
+        ["i"] = {
+            ["<C-l>"] = pa_actions.hottest_callees,
+            ["<C-h>"] = pa_actions.hottest_callers,
+        },
+
+        ["n"] = {
+            -- TODO: set sensible defaults...
+        }
+    }
+}
+
+--- Attaches this plugins' mappings to the finders.
+-- @param _ Buffer number of the prompt (we don't need this).
+-- @param map Function that telescope passes for us to set mappings.
+-- @return Always true to signal that we don't want to remove the default mappings.
+function popts.attach_mappings(_, map)
+    for mode, tbl in pairs(popts.mappings) do
+        for key, fun in pairs(tbl) do
+            map(mode, key, fun)
+        end
+    end
+
+    return true
+end
+
 --- Annotates a given buffer assuming it displays the given file.
 -- @param bufnr Buffer number of buffer to annotate.
 -- @param file File that should be used to look up annotations.
@@ -95,11 +165,14 @@ local function annotate_file(bufnr, file)
 end
 
 --- Opens telescope finder to find hottest lines in the project.
-local function find_hottest_lines()
+-- @param opts Telescope options to override defaults from this plugin.
+local function find_hottest_lines(opts)
+    opts = opts or {}
+
     perfanno.with_event(function()
         local entries, total_count = find_hottest.hottest_lines_table(config.selected_event)
 
-        pickers.new({}, {
+        pickers.new(vim.tbl_deep_extend("force", popts, opts), {
             prompt_title = "",
             finder = finder_from_table(entries, total_count),
             sorter = tconf.file_sorter{},
@@ -109,29 +182,32 @@ local function find_hottest_lines()
 end
 
 --- Opens telescope finder to find hottest symbols (functions) in the project.
-local function find_hottest_symbols()
+-- @param opts Telescope options to override defaults from this plugin.
+local function find_hottest_symbols(opts)
+    opts = opts or {}
+
     perfanno.with_event(function()
         local entries, total_count = find_hottest.hottest_symbols_table(config.selected_event)
 
-        pickers.new({}, {
+        pickers.new(vim.tbl_deep_extend("force", popts, opts), {
             prompt_title = "",
             finder = finder_from_table(entries, total_count),
             sorter = tconf.file_sorter{},
-            previewer = annotated_previewer(annotate_file)
+            previewer = annotated_previewer(annotate_file),
         }):find()
     end)
 end
 
 --- Opens telescope finder to find the hottest callers of the function containing the cursor.
-local function find_hottest_callers_function()
+-- @param opts Telescope options to override defaults from this plugin.
+local function find_hottest_callers_function(opts)
+    opts = opts or {}
+
     perfanno.with_event(function()
         local entries, total_count = find_hottest.hottest_callers_function_table(config.selected_event)
+        if not entries then return end
 
-        if not entries then
-            return
-        end
-
-        pickers.new({}, {
+        pickers.new(vim.tbl_deep_extend("force", popts, opts), {
             prompt_title = "",
             finder = finder_from_table(entries, total_count),
             sorter = tconf.file_sorter{},
@@ -141,15 +217,15 @@ local function find_hottest_callers_function()
 end
 
 --- Opens telescope finder to find the hottest callers of current visual selection.
-local function find_hottest_callers_selection()
+-- @param opts Telescope options to override defaults from this plugin.
+local function find_hottest_callers_selection(opts)
+    opts = opts or {}
+
     perfanno.with_event(function()
         local entries, total_count = find_hottest.hottest_callers_selection_table(config.selected_event)
+        if not entries then return end
 
-        if not entries then
-            return
-        end
-
-        pickers.new({}, {
+        pickers.new(vim.tbl_deep_extend("force", popts, opts), {
             prompt_title = "",
             finder = finder_from_table(entries, total_count),
             sorter = tconf.file_sorter{},
@@ -158,11 +234,23 @@ local function find_hottest_callers_selection()
     end)
 end
 
+--- Sets up plugin by overriding options with user provided ones.
+local function setup(opts)
+    popts.mappings = vim.tbl_deep_extend("force", popts.mappings, tconf.mappings)
+    popts = vim.tbl_deep_extend("force", popts, opts)
+end
+
 return telescope.register_extension {
+    setup = setup,
+
     exports = {
+        -- Finders
         find_hottest_lines = find_hottest_lines,
         find_hottest_symbols = find_hottest_symbols,
         find_hottest_callers_function = find_hottest_callers_function,
         find_hottest_callers_selection = find_hottest_callers_selection,
+
+        -- Actions
+        actions = pa_actions,
     }
 }
